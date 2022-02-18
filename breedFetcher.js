954f8bd5e235397281091f264d750ed12f3b6639
vagrant <tpirozzini@gmail.com>
2022-02-18 00:22:45 +0000

    add refactored json_the_cat and test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,25 @@
-const breedName = process.argv[2];
 const request = require("request");
 
-const URL = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
 
-request(URL, (error, response, body) => {
-  //console.log('error:', error);
-  //console.log('statusCode:', response && response.statusCode);
-  //console.log('body:', body);
-  const data = JSON.parse(body);
-  //console.log(data);
-  //console.log(typeof data);
-  if (error === undefined) {
-    console.log('error:', error);
-    return;
-  }
-  if (!data[0]) {
-    console.log("Breed not found.");
-  } else {
-    console.log(data[0].description);
-  }
-});
+  request(URL, (error, response, body) => {
+    // console.log('error:', error);
+    // console.log('statusCode:', response && response.statusCode);
+    // console.log('body:', body);
+    const data = JSON.parse(body);
+    if (error === undefined) {
+      callback(error, null);
+      return;
+    }
+    if (!data[0]) {
+      callback(null, "Breed not found.");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
 
